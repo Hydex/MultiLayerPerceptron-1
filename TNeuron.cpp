12a78bbd79483d2410c2386b13e18863ee046ac2
Leo <lev.mishin1992@gmail.com>
2012-11-21 19:36:35 +0400

    formatted code and fixed minor bugs

--- TNeuron.cpp
+++ TNeuron.cpp
@@ -5,7 +5,7 @@ TNeuron::TNeuron(int countOfSynWeights)
 {
 	double tmp = 1.0 / (100 + countOfSynWeights * countOfSynWeights);
 	Q_ASSERT(tmp);
-	for(int i = 0; i < countOfSynWeights; i++) {
+    for (int i = 0; i < countOfSynWeights; i++) {
 		mSynapticWeights << ((qrand()%100+1) - 50)/10.0;
 	}
 	Q_ASSERT(mSynapticWeights.size() == countOfSynWeights);
@@ -14,7 +14,7 @@ TNeuron::TNeuron(int countOfSynWeights)
 TNeuron::TNeuron(const QDomElement &neuron)
 {
 	QDomElement weight = neuron.firstChild().toElement();
-	while(!weight.isNull()) {
+    while (!weight.isNull()) {
 		double newWeight = weight.attribute("value").toDouble();
 		mSynapticWeights << newWeight;
 		weight = weight.nextSibling().toElement();
@@ -43,7 +43,7 @@ double TNeuron::getResult(const QVector<double> &input
 						  , ExpSigmoidal &activationFunction)
 {
 	double sum = 0;
-	for(int i = 0; i < input.size(); i++) {
+    for (int i = 0; i < input.size(); i++) {
 		sum += input.at(i) * mSynapticWeights.at(i);
 	}
 	double out = activationFunction.calculate(sum);
@@ -57,7 +57,7 @@ QVector<double> TNeuron::learn(double dj, QVector<double> input)
 {
 	QVector<double> correction;
 	Q_ASSERT(input.size() == mSynapticWeights.size());
-	for(int i = 0; i < input.size(); i++) {
+    for (int i = 0; i < input.size(); i++) {
 		mSynapticWeights[i] += -dj * nn * input.at(i);
 		correction << mSynapticWeights.at(i) * dj;
 	}
@@ -69,7 +69,7 @@ QDomElement TNeuron::save(QDomDocument &mlp, int neuronNumber)
 	QDomElement neuron = mlp.createElement("neuron");
 	neuron.setAttribute("neuronNumber", neuronNumber);
 	neuron.setAttribute("countOfWeights", mSynapticWeights.size());
-	for(int i = 0; i < mSynapticWeights.size(); i++) {
+    for (int i = 0; i < mSynapticWeights.size(); i++) {
 		QDomElement weight = mlp.createElement("weight");
 		weight.setAttribute("number", i+1);
 		weight.setAttribute("value", mSynapticWeights.at(i));
